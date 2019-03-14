96d63181f4e6d99d481672bf27bbb7dbc4a1e95e
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-03-14 09:12:09 +0200

    väiksed kommentaarid ja asjad

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,38 +1,47 @@
+// defineerime
 describe('Address Book', function () {
     var addressBook,
         thisContact;
-
+    // kutsume äädressbuuki ja disscontacti valja!
     beforeEach(function() {
         addressBook = new AddressBook();
         thisContact = new Contact();
     });
-
+    // itime veica, et saab kontakteeruda
     it('should be able to contact', function () {
+        // lisame kontakte
         addressBook.addContact(thisContact);
-
+        // ekspekteerime
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
-
+    // itime veica veel, et saaks kustutada
     it('should be able to delete a contact', function() {
+        // lisa
         addressBook.addContact(thisContact);
+        // kustuta
         addressBook.deleteContact(0);
-
+        // ekspekteerime, et sõber meil kustutatud
         expect(addressBook.getContact(0)).not.toBeDefined();
     });
 });
-
+// teeme asünkrooni värki ka veica
 describe('Async Address Book', function() {
+    // itime veica, et peab krabama initsiaalsed kontaktid
     it('should grab initial contacts', function() {
+        // teeme variable milleks määrame uue aadressiraamatu!!!
         var addressBook = new AddressBook();
-
+        // ennem funktsiooni on asjaga juba finito
         beforeEach(function(done) {
+            // väike asünkroonne lisamine meil siin tehtud
             addressBook.getInitialContacts(function() {
                 done();
             });
         });
-
+        // itime veica, et peab krabama initsiaalsed kontaktid
         it('should grab inital contacts', function(done) {
+            // lisamine === true
             expect(addressBook.initialComplete).toBe(true);
+            // donso alfonso
             done();
         });
     });
--- spec/PlayerSpec.js
+++ spec/PlayerSpec.js
@@ -6,7 +6,7 @@ describe("Player", function() {
     player = new Player();
     song = new Song();
   });
-
+  
   it("should be able to play a Song", function() {
     player.play(song);
     expect(player.currentlyPlayingSong).toEqual(song);
