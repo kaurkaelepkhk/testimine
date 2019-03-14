ac00095febc3c51577b80ea7654aac7716715ff3
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-03-14 09:01:55 +0200

    Maiki solutsioon

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,10 +1,10 @@
-describe('Address Book', function() {
-    interface('should beable to add a contact', function() {
+describe('Address Book', function () {
+    it('should be able to contact', function () {
         var addressBook = new AddressBook(),
-        thisContact = new thisContact();
+            thisContact = new Contact();
 
         addressBook.addContact(thisContact);
 
         expect(addressBook.getContact(0)).toBe(thisContact);
     });
-}); 
+});
