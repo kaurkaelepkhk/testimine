a6e3ef0dd6b9756b1ee466b7d643ee5cfa278431
kaurkaelepkhk <kaur.kaelep@khk.ee>
2019-03-14 09:03:01 +0200

    Teine spekulatsioonimatrix

--- src/AddressBook.js
+++ src/AddressBook.js
@@ -8,4 +8,8 @@ AddressBook.prototype.addContact = function(contact) {
 
 AddressBook.prototype.getContact = function(index) {
     return this.contacts[index];
-} 
+}
+
+AddressBook.prototype.deleteContact = function(index) {
+    this.contacts.splice(index, 1);
+}
